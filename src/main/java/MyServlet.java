fd2fd4432533a04c1ab356ea93df8f7427af74e3
Marble2099 <batbin@mail.ru>
2022-09-20 14:55:21 +0300

    Working version

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,3 +1,6 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +13,14 @@ public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     static int val = 1;
+    private static final Logger logger = LogManager.getLogger(MyServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         String name = request.getParameter("firstName");
         String countValue = request.getParameter("lastName");
         String tolog = request.getParameter("toLog");
-        
+
         String[] parts = name.split("_");
         int min = Integer.parseInt(parts[0]);
         int max = Integer.parseInt(parts[1]);
@@ -23,22 +28,23 @@ public class MyServlet extends HttpServlet {
 
        val += Integer.parseInt(countValue);
 
-        String filepath = "C:\\Users\\user.DESKTOP-AFJDH5T\\IdeaProjects\\WebApp\\src\\main\\log.txt";
+        //String filepath = "C:\\Users\\user.DESKTOP-AFJDH5T\\IdeaProjects\\WebApp\\src\\main\\log.txt";
         //String text = "abracadabra!";
 
-        try {
-            FileWriter writer = new FileWriter(filepath, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            bufferedWriter.write(tolog);
-            bufferedWriter.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+//        try {
+//            FileWriter writer = new FileWriter(filepath, true);
+//            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+//            bufferedWriter.write(tolog);
+//            bufferedWriter.close();
+//        } catch (IOException e) {
+//            System.out.println(e);
+//        }
         PrintWriter out = response.getWriter();
         //out.println("Hello from Servlet!.GET" + name + lastname);
         out.println("randValue=" + random_int);
         out.println("counterValue= " + val);
         out.println(tolog);
+        logger.info(tolog);
         out.close();
     }
 
