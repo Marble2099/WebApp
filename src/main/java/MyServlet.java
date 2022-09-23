73632bf4a4890ef038d2b3c923319afde9087e16
Marble2099 <batbin@mail.ru>
2022-09-23 13:16:14 +0300

    Working version

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -15,33 +15,57 @@ public class MyServlet extends HttpServlet {
     static int val = 1;
     private static final Logger logger = LogManager.getLogger(MyServlet.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("firstName");
-        String countValue = request.getParameter("lastName");
-        String tolog = request.getParameter("toLog");
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String name = request.getParameter("firstName");
+//        String countValue = request.getParameter("lastName");
+//        String tolog = request.getParameter("toLog");
+//
+//        String[] parts = name.split("_");
+//        int min = Integer.parseInt(parts[0]);
+//        int max = Integer.parseInt(parts[1]);
+//        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+//
+//       val += Integer.parseInt(countValue);
+//
+//        PrintWriter out = response.getWriter();
+//        //out.println("Hello from Servlet!.GET" + name + lastname);
+//        out.println("randValue=" + random_int);
+//        out.println("counterValue= " + val);
+//        out.println(tolog);
+//        logger.info(tolog);
+//        out.close();
+//    }
 
-        String[] parts = name.split("_");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.setCharacterEncoding("UTF-8"); // чтобы в POST запросе корректно отображались русские буквы
+//        String name = request.getParameter("firstName");
+//        String lastname = request.getParameter("lastName");
+//        PrintWriter out = response.getWriter();
+//        out.println("Hello from Servlet!.POST" + name + lastname);
+//        out.close();
+        request.setCharacterEncoding("UTF-8");
+
+        String randValue = request.getParameter("RandValue");
+        String countValue = request.getParameter("CountValue");
+        String tolog = request.getParameter("ToLog");
+
+        String[] parts = randValue.split("_");
         int min = Integer.parseInt(parts[0]);
         int max = Integer.parseInt(parts[1]);
         int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
 
-       val += Integer.parseInt(countValue);
+        val += Integer.parseInt(countValue);
 
+        response.setContentType("text/html;charset7=utf-8");
         PrintWriter out = response.getWriter();
         //out.println("Hello from Servlet!.GET" + name + lastname);
+        out.println("<h3>POST method</h3>");
         out.println("randValue=" + random_int);
+        out.print("<br>");
         out.println("counterValue= " + val);
+        out.print("<br>");
         out.println(tolog);
         logger.info(tolog);
-        out.close();
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8"); // чтобы в POST запросе корректно отображались русские буквы
-        String name = request.getParameter("firstName");
-        String lastname = request.getParameter("lastName");
-        PrintWriter out = response.getWriter();
-        out.println("Hello from Servlet!.POST" + name + lastname);
         out.close();
     }
 }
